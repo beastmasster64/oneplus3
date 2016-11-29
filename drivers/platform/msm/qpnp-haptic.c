flar2/bullhead

msm: qpnp-haptic: disable vibration when vmax_mv_strong is set to min

drivers/platform/msm/qpnp-haptic.c
@@ -1637,6 +1637,12 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 		}
 		hap->state = 0;
 	} else {
+
+		if (hap->vmax_mv_haptic == QPNP_HAP_VMAX_MIN_MV) {
+			mutex_unlock(&hap->lock);
+			return;
+		}
+
 		if (value >= CALL_ALARM_TIME_THRESHOLD
 				&& hap->vmax_mv_ind > 0
 				&& hap->vmax_mv != hap->vmax_mv_ind){

beastmasster64/oneplus3

drivers/platform/msm/qpnp-haptic.c
@@ -1610,6 +1610,12 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 		}
 		hap->state = 0;
 	} else {
+
+		if (hap->vmax_mv == QPNP_HAP_VMAX_MIN_MV) {
+			mutex_unlock(&hap->lock);
+			return;
+		}
+
 		value = (value > hap->timeout_ms ?
 				 hap->timeout_ms : value);
 		hap->state = 1;